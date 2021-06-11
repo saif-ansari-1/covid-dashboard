39eb9ae4cec8c9204aa3dc753f3233ece900c553
saif-ansari-1 <ansarisaif04@gmail.com>
2021-06-12 03:48:40 +0530

    test

---
+++ src/test/java/com/example/coviddashboard/CovidDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coviddashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
